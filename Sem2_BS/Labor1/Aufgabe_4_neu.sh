3bf34c482989854ebd31b918573da62cf1a138dc
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-10-11 19:17:23 +0200

    Update Aufgabe_4_neu.sh

--- Sem2_BS/Labor1/Aufgabe_4_neu.sh
+++ Sem2_BS/Labor1/Aufgabe_4_neu.sh
@@ -11,14 +11,26 @@ DSTRING= ``
 
 # Gibt den Datum aus
 #DSTRING=`grep -rI Date: eml/ | cut -d ":" -f 3`
-
 DSTRING=`grep -rI Date: Beispielmails/ | cut -d ":" -f 3`
+
+# Mon, 19 Mar 2018 15
+_yea=$(echo $DSTRING | cut -d " " -f 4)
+_mnt=$(echo $DSTRING | cut -d " " -f 3)
+_day=$(echo $DSTRING | cut -d " " -f 2)
+_hrs=$(echo $DSTRING | cut -d " " -f 5)
 
 _min=`grep -rI Date: Beispielmails/ | cut -d ":" -f 4`
 _sec=`grep -rI Date: Beispielmails/ | cut -d ":" -f 5`
- 
-echo "$_min"
-echo "$_sec"
+
+echo "----------------------------"
+echo "Year: $_yea"
+echo "Mnth: $_mnt"
+echo "Day : $_day"
+echo "Hour: $_hrs"
+
+echo "Min : $_min"
+echo "Sec : $_sec"
+echo "----------------------------"
 
 # Kontrolle ob DSTRING die Liste der Datum enstpicht 
 #echo ${DSTRING}
