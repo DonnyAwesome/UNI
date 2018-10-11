06c9dc924ba1964eaf9834e818c3489ac77a2f8b
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-10-11 18:43:33 +0200

    Create Aufgabe_3.sh

---
+++ Sem2_BS/Labor1/Aufgabe_3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ 
+DSTRING= ``
+#grep
+#r -> recursive
+#n -> get the line Number if found
+#I -> no Binary file
+#o -> only matching
+ 
+# Gibt den Dateinamen aus
+DSTRING=`grep -rI Date: ${PWD}`
+
+# oder geht auch mit
+#DSTRING=`grep -rI Date: *`
