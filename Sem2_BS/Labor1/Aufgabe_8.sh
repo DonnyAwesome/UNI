ddd347d7a234f0141ecf04756d374b2bbcbc79d1
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-10-11 19:42:32 +0200

    Create Aufgabe_8.sh

---
+++ Sem2_BS/Labor1/Aufgabe_8.sh
@@ -0,0 +1,4 @@
+# Wie können sie erreichen, dass sie das Shellskript storemails alleine durch Aufrufendes Namens starten können? 
+
+#Die Umgebungsvar. PATH  ändern und den Verzeichnis von Storemails.sh hinzufügen
+#Oder Stotrmails.sh in /bin kopieren 
