fb1b17d7a9a3f4a81a208697c428b182400b36a7
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-10-11 18:45:03 +0200

    Create Aufgabe_4.sh

---
+++ Sem2_BS/Labor1/Aufgabe_4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DSTRING= ``
+#grep
+#r -> recursive
+#n -> get the line Number if found
+#I -> no Binary file
+#o -> only matching
+ 
+# Gibt den Dateinamen aus
+#DSTRING=`grep -rI Date: * | cut -d ":" -f 1`
+#email.eml storemail.sh storemail.sh storemail.sh storemail.sh~
+ 
+# Gibt den Müll aus
+#DSTRING=`grep -rI Date: * | cut -d ":" -f 2`
+#Date DSTRING=`grep -rI Date DSTRING=`grep -rI Date #DSTRING=`grep -rI Date DSTRING=`grep -rI Date #DSTRING=`grep -rI Date #DSTRING=`grep -rI Date
+ 
+# Gibt den Datum aus
+#DSTRING=`grep -rI Date: eml/ | cut -d ":" -f 3`
+DSTRING=`grep -rI Date: Beispielmails/ | cut -d ":" -f 3`
+ 
+# Kontrolle ob DSTRING die Liste der Datum enstpicht 
+#echo ${DSTRING}
+ 
+for a in "${DSTRING}"; do
+    echo "$a"
+done
