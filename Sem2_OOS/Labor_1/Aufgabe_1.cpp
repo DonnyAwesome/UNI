c74362b2d8f7c3d278331b89869f7a8e07e72f30
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-10-24 11:59:21 +0200

    Create Aufgabe_1.cpp

---
+++ Sem2_OOS/Labor_1/Aufgabe_1.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+	double d = 1.234;
+	cout << "Nr  Oct  Hex String  Fixed     Scientific " << endl;
+	for (int k = 1; k < 11; k++) {
+		
+		char a = '*';
+		size_t c = k % 5 + 1;
+
+		cout << setw(3) << right << dec << k;
+		cout << setw(4) << right << oct << k;
+		cout << setw(4) << right << "0x" << hex << k << " ";
+		//cout << setfill('+') << setw(7)  << left << string(c, a) << " ";
+		cout << setw(7) << left << string(c, a) << " ";
+		cout << setw(8) << right << setprecision(3) << fixed << d << " ";
+		cout << setw(11) << right << scientific << d;
+		d = d*-2;
+		cout << endl;
+	}
+	//system("pause");
+	return 0;
+}
